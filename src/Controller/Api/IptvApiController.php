7f18b7dbc225c1ad41b2deb19a48298e2e6702c0
rodriguembah13 <rodriguembah13@gmail.com>
2022-11-03 12:28:52 +0100

    first commit

--- src/Controller/Api/IptvApiController.php
+++ src/Controller/Api/IptvApiController.php
@@ -10,6 +10,7 @@ use App\Repository\CardPendingRepository;
 use App\Repository\CardRepository;
 use App\Repository\CustomerRepository;
 use App\Service\EndpointService;
+use Doctrine\ORM\EntityManagerInterface;
 use FOS\RestBundle\Controller\AbstractFOSRestController;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use Psr\Log\LoggerInterface;
@@ -24,22 +25,26 @@ class IptvApiController extends AbstractFOSRestController
     private $bouquetRepository;
     private $cardRepository;
     private $cardpendingRepository;
+    private $doctrine;
 
     /**
      * IptvApiController constructor.
      * @param CardPendingRepository $cardPendingRepository
+     * @param EntityManagerInterface $entityManager
      * @param CardRepository $cardRepository
      * @param BouquetRepository $bouquetRepository
      * @param LoggerInterface $logger
      * @param EndpointService $endpointService
      */
-    public function __construct(CardPendingRepository $cardPendingRepository,CardRepository $cardRepository,BouquetRepository $bouquetRepository,LoggerInterface $logger, EndpointService $endpointService)
+    public function __construct(CardPendingRepository $cardPendingRepository, EntityManagerInterface $entityManager,
+                                CardRepository $cardRepository, BouquetRepository $bouquetRepository, LoggerInterface $logger, EndpointService $endpointService)
     {
         $this->logger = $logger;
         $this->endpointsService = $endpointService;
-        $this->bouquetRepository=$bouquetRepository;
-        $this->cardRepository=$cardRepository;
-        $this->cardpendingRepository=$cardPendingRepository;
+        $this->bouquetRepository = $bouquetRepository;
+        $this->cardRepository = $cardRepository;
+        $this->cardpendingRepository = $cardPendingRepository;
+        $this->doctrine = $entityManager;
     }
 
 
@@ -65,6 +70,7 @@ class IptvApiController extends AbstractFOSRestController
         $view = $this->view($values, Response::HTTP_OK, []);
         return $this->handleView($view);
     }
+
     /**
      * @Rest\Get("/v1/cardstatus", name="api_allcard")
      * @return Response
@@ -72,15 +78,16 @@ class IptvApiController extends AbstractFOSRestController
     public function getAllcard()
     {
         $data = $this->cardRepository->findAll();
-      /*  $values = [
-            'card_id'=>$data['card_id'],
-            'card_status'=>$data['card_status'],
-            'send_or_not'=>$data['send_or_not'],
-            'expired_time'=>$data['expired_time'],
-        ];*/
+        /*  $values = [
+              'card_id'=>$data['card_id'],
+              'card_status'=>$data['card_status'],
+              'send_or_not'=>$data['send_or_not'],
+              'expired_time'=>$data['expired_time'],
+          ];*/
         $view = $this->view($data, Response::HTTP_OK, []);
         return $this->handleView($view);
     }
+
     /**
      * @Rest\Get("/v1/activatecard", name="api_activatecard")
      * @return Response
@@ -89,24 +96,37 @@ class IptvApiController extends AbstractFOSRestController
     {
         $data = $this->cardpendingRepository->findOneByFirst();
         $values = [
-            'id'=>$data->getId(),
-            'card_id'=>$data->getCardid(),
-            'card_status'=>$data->getCardstatus(),
-            'send_or_not'=>$data->getSendornot(),
-            'expired_time'=>$data->getExpiredtime()->format('Y-m-d h:m:s'),
+            'id' => $data->getId(),
+            'card_id' => $data->getCardid(),
+            'card_status' => $data->getCardstatus(),
+            'send_or_not' => $data->getSendornot(),
+            'expired_time' => $data->getExpiredtime()->format('Y-m-d h:m:s'),
         ];
         $view = $this->view($values, Response::HTTP_OK, []);
         return $this->handleView($view);
     }
-     /**
+
+    /**
      * @Rest\Post("/v1/reponseactivatecard", name="api_activatecardresponse")
      * @return Response
      */
     public function activateCardResponse(Request $request)
     {
-       $res = json_decode($request->getContent(), true);
-       $this->logger->info("----------activation ok---------------");
-        $data = $res;
+        $res = json_decode($request->getContent(), true);
+        $this->logger->info("----------activation ok---------------");
+        $pending = $this->cardpendingRepository->find($res['id']);
+        if ($res['code'] == 0) {
+            $pending->setCardstatus(0);
+            $pending->setIsdelete(true);
+            $this->doctrine->remove($pending);
+        } else {
+            $pending->setCardstatus(1);
+            $pending->setIsdelete(false);
+        }
+        $this->logger->info($res['code']);
+        $this->logger->info($res['cardid']);
+        $this->logger->info($res['id']);
+        $this->doctrine->flush();
         $view = $this->view([], Response::HTTP_OK, []);
         return $this->handleView($view);
     }
